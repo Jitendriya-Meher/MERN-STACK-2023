203cafbd7ab50c591cfc3f7487ce044cf4e94378
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2023-12-03 12:36:00 +0530

    getting started with frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Service from "./pages/Service";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="">
+
+
+      <Routes>
+
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/service" element={<Service></Service>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
